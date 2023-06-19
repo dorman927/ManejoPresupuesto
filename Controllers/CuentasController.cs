12c032fce11a2641abfb12aec8059304c7c61628
Jordan Marte <jordan.marte@bionuclear.com.do>
2023-06-19 08:56:27 -0400

    Subor creacion de cuentas

---
+++ Controllers/CuentasController.cs
@@ -0,0 +1,31 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+	public class CuentasController :Controller
+	{
+		private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+		private readonly IServiciosUuarios serviciosUuarios;
+
+		public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas,
+			                     IServiciosUuarios serviciosUuarios)
+		{
+			this.repositorioTiposCuentas = repositorioTiposCuentas;
+			this.serviciosUuarios = serviciosUuarios;
+		}
+
+		[HttpGet]
+		public  async Task<IActionResult> Crear()
+		{
+			var usurioId = serviciosUuarios.ObtenerUsuarioId();
+			var tiposCuentas = await repositorioTiposCuentas.Obtener(usurioId);
+			var modelo = new CuentaCreacionViewModel();
+
+			modelo.TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+			return View(modelo);
+		}
+	}
+}
---
+++ Models/Cuenta.cs
@@ -0,0 +1,22 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ManejoPresupuesto.Models
+{
+	public class Cuenta
+	{
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El Campo {0} es requerido")]
+        [StringLength(maximumLength:50)]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        [Display(Name = "Tipo Cuenta")]
+        public int TipoCuentaId { get; set; }
+        public decimal Balance { get; set; }
+        [StringLength(maximumLength:1000)]
+        public string Descripcion { get; set; }
+
+
+    }
+}
